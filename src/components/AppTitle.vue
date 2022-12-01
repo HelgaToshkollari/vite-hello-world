2c1cc2a526859e9f32c1f18a53517b294e68fb7e
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-12-01 15:32:02 +0100

    adds AppTitle

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+<script>
+
+export default {
+	
+    data(){
+    return{
+      title: "La mia prima app con Vite!"
+    };
+  },
+ 
+};
+</script>
+
+<template>
+	<div class="container">
+
+        <h1>{{ title }}</h1>
+
+    </div>
+	
+	
+</template>
+<style></style>
